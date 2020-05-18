2301ea06e8c6d2f76269b7584a18b19acd2864e7
frazier_g <garyfrazier95@yahoo.com>
2020-05-18 15:58:16 -0400

    gitignore

--- VaporEngine/src/VaporEngine/Application.cpp
+++ VaporEngine/src/VaporEngine/Application.cpp
@@ -7,14 +7,22 @@
 
 namespace VaporEngine
 {
+#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Application::~Application()
 	{
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		VE_CORE_INFO("{0}", e);
 	}
 
 	void Application::Run()
